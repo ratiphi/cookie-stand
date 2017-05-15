0fb26362134e0c81f93eaf5f3f5dd568df393966
Tim Turner <timmyturner@me.com>
2017-05-15 13:16:52 -0700

    edit to app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+var firstAndPike = {
+  min: 23,
+  max: 65,
+  avg: 6.3
+};
+
+var seatacAirport = {
+  min: 3,
+  max: 24,
+  avg: 1.2
+};
+
+var seattleCenter = {
+  min: 11,
+  max: 38,
+  avg: 3.7
+};
+
+var capitolHill = {
+  min: 20,
+  max: 38,
+  avg: 2.3
+};
+
+var alki = {
+  min: 2,
+  max: 16,
+  avg: 4.6
+};
