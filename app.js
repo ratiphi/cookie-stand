aab27fdde4671669168cad5fdf2fd3afaca47e62
Tim Turner <timmyturner@me.com>
2017-05-16 11:14:19 -0700

    saving constructor work

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+function StoreLocation(name, minCust, maxCust, avgPerHour) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgPerHour = avgPerHour;
+}
+
+StoreLocation.prototype.randCust = function() {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+};
+
+StoreLocation.prototype.randHourlySales = function() {
+  var elList = document.createElement('ul');
+  var elstoreName = document.createElement('p');
+  var arrRandHourlySales = [];
+  var totalCookies = 0;
+
+  for (var i = 6; i < 12; i++) {
+    var hourlyCookies = Math.floor(this.randCust() * this.avgPerHour);
+    arrRandHourlySales.push('<li>' + i + 'am: ' + hourlyCookies + ' cookies' + '</li>');
+    totalCookies = totalCookies + hourlyCookies;
+  }
+  hourlyCookies = Math.floor(this.randCust() * this.avgPerHour);
+  arrRandHourlySales.push('<li>' + 12 + 'pm: ' + hourlyCookies + ' cookies' + '</li>');
+  totalCookies = totalCookies + hourlyCookies;
+  for (i = 1; i < 9; i++) {
+    hourlyCookies = Math.floor(this.randCust() * this.avgPerHour);
+    arrRandHourlySales.push('<li>' + i + 'pm: ' + hourlyCookies + ' cookies' + '</li>');
+    totalCookies = totalCookies + hourlyCookies;
+  }
+  arrRandHourlySales.push('<li>' + 'Total: ' + totalCookies + ' cookies' + '</li>');
+  var elFullList = arrRandHourlySales.join('');
+  elstoreName.innerHTML = this.name;
+  elList.innerHTML = elFullList;
+  document.body.appendChild(elstoreName);
+  document.body.appendChild(elList);
+};
+
+// var firstAndPike = new StoreLocation('1st and Pike', 23, 65, 6.3);
+
+
 var firstAndPike = {
   name: '1st and Pike',
   minCust: 23,
